6968dab25ff4d1945a4cfaf28817994c5667be07
nguyentient2009a <tienndth2008051@fpt.edu.vn>
2021-08-11 15:11:19 +0700

    client

---
+++ resources/views/client.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<form action="">
+    id:
+    <input type="text">
+    name:
+    <input type="text">
+    price:
+    <input type="text">
+    avatar:
+    <input type="text">
+    <input type="submit">
+</form>
+</body>
+</html>
